133b254044489f560201d42b541e9c8120eaffa1
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2023-07-02 16:54:49 -0400

    feat: cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import esLocale from '@fullcalendar/core/locales/es';
 import { CarrerService } from './services/carrer.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { PatientService } from './services/patient.service';
 import { DoctorService } from './services/doctor.service';
 import { TurnService } from './services/turn.service';
@@ -28,8 +28,7 @@ export class AppComponent implements OnInit {
     plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
     headerToolbar: {
       left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+      right: 'title',
     },
     initialView: 'dayGridMonth',
     locale: esLocale,
@@ -44,17 +43,45 @@ export class AppComponent implements OnInit {
   };
 
   formDoctor = this.fb.group({
-    patient: [''],
-    carrer: [''],
-    doctor: [''],
-    turn: [''],
+    patient: ['', Validators.required],
+    carrer: ['', Validators.required],
+    doctor: ['', Validators.required],
+    turn: ['', Validators.required],
   });
 
   currentEvents: EventInput[] = [
     {
       allDay: true,
-      backgroundColor: 'red',
-      date: '2023-06-29T10:00:00',
+      backgroundColor: '#acddc7',
+      display: 'background',
+      daysOfWeek: [1, 3, 5],
+    },
+    {
+      date: '2023-07-03',
+      backgroundColor: '#cfe2ff',
+      borderColor: '#9ec5fe',
+      display: 'auto',
+      title: `Disponibles: 15`,
+      textColor: '#052c65',
+      extendedProps: {
+        dates: [
+          '08:30',
+          '08:50',
+          '09:10',
+          '09:30',
+          '09:50',
+          '10:10',
+          '10:30',
+          '10:50',
+          '11:10',
+          '11:30',
+          '11:50',
+          '12:10',
+          '12:30',
+          '12:50',
+          '13:10',
+        ],
+      },
     },
   ];
 
@@ -65,7 +92,13 @@ export class AppComponent implements OnInit {
   turns: any[] = [];
   dates: any[] = [];
 
-
+  dias = {
+    dias: [
+      { id: 1, nombre: 'Lunes', index: 1 },
+      { id: 3, nombre: 'Miercoles', index: 3 },
+      { id: 5, nombre: 'Viernes', index: 5 },
+    ],
+  };
 
   constructor(
     private changeDetector: ChangeDetectorRef,
@@ -73,8 +106,7 @@ export class AppComponent implements OnInit {
     private carrerService: CarrerService,
     private doctorService: DoctorService,
     private patientService: PatientService,
-    private turnService: TurnService,
-    
+    private turnService: TurnService
   ) {}
   ngOnInit(): void {
     this.patientService.getPatients().subscribe({
@@ -86,8 +118,6 @@ export class AppComponent implements OnInit {
 
     this.formDoctor.get('carrer')?.valueChanges.subscribe({
       next: (res: any) => {
-        console.log(res);
-        
         this.doctorsByCarrerId(res);
         this.daysByCarrerId(res);
       },
@@ -96,44 +126,77 @@ export class AppComponent implements OnInit {
     this.formDoctor.get('doctor')?.valueChanges.subscribe({
       next: (res: any) => {
         this.daysByDoctorId(res);
-        this.turnsByDoctor(res)
+        this.turnsByDoctor(res);
       },
     });
 
     this.formDoctor.get('turn')?.valueChanges.subscribe({
       next: (res: any) => {
-        this.datesByTurnId(res)
+        this.datesByTurnId(res);
       },
     });
   }
 
   datesByTurnId(id: number) {
     this.turnService.datesByTurn(id).subscribe({
-      next: (res: any) => this.dates = res.fechas
+      next: (res: any) => {
+        let dates = res.fechas;
+        dates.forEach((element: any) => {
+          this.currentEvents.push({
+            date: element.data,
+            backgroundColor: '#cfe2ff',
+            borderColor: '#9ec5fe',
+            display: 'auto',
+            title: `Disponibles: ${element.nroReservasDisponibles}`,
+            textColor: '#052c65',
+            extendedProps: {
+              dates: element.horarios,
+            },
+          });
+        });
+      },
     });
   }
 
   doctorsByCarrerId(id: number) {
     this.doctorService.doctorsByCarrerId(id).subscribe({
-      next: (res) => this.doctors = res.doctores
+      next: (res) => (this.doctors = res.doctores),
     });
   }
 
   turnsByDoctor(id: number) {
     this.turnService.turnsByDoctor(id).subscribe({
-      next: (res: any) => this.turns = res.turnos
-    });    
+      next: (res: any) => (this.turns = res.turnos),
+    });
   }
 
   daysByCarrerId(id: number) {
     this.carrerService.daysByCarrer(id).subscribe({
-      next: (res: any) => this.days = res.dias
+      next: (res: any) => {
+        this.days = res.dias;
+        this.currentEvents.pop();
+        this.currentEvents.push({
+          allDay: true,
+          backgroundColor: '#acddc7',
+          display: 'background',
+          daysOfWeek: this.days.map((el) => el.index),
+        });
+      },
     });
   }
 
   daysByDoctorId(id: number) {
     this.doctorService.daysByDoctor(id).subscribe({
-      next: (res: any) => this.days = res.dias
+      next: (res: any) => {
+        this.days = res.dias;
+        this.currentEvents.pop();
+        this.currentEvents.push({
+          allDay: true,
+          backgroundColor: '#acddc7',
+          display: 'background',
+          daysOfWeek: this.days.map((el) => el.index),
+        });
+      },
     });
   }
 
@@ -147,38 +210,22 @@ export class AppComponent implements OnInit {
   }
 
   handleDateSelect(selectInfo: DateSelectArg) {
-    // const title = prompt('Please enter a new title for your event');
-    console.log(selectInfo);
-
     const calendarApi = selectInfo.view.calendar;
 
     calendarApi.unselect(); // clear date selection
-
-    // if (title) {
-    //   calendarApi.addEvent({
-    //     id: createEventId(),
-    //     title,
-    //     start: selectInfo.startStr,
-    //     end: selectInfo.endStr,
-    //     allDay: selectInfo.allDay,
-    //   });
-    // }
   }
 
   handleEventClick(clickInfo: EventClickArg) {
-    console.log(clickInfo);
-
-    // if (
-    //   confirm(
-    //     `Are you sure you want to delete the event '${clickInfo.event.title}'`
-    //   )
-    // ) {
-    //   clickInfo.event.remove();
-    // }
+    console.log(clickInfo.event.extendedProps);
+    this.dates = clickInfo.event.extendedProps['dates'];
+    console.log(this.dates);
   }
 
   handleEvents(events: EventApi[]) {
-    // this.currentEvents = events;
     this.changeDetector.detectChanges();
+  }
+
+  onSubmit() {
+    console.log('Registrar');
   }
 }
